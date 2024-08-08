28cc9feffce8c8fd82df47ee4a00d5cc2de1701f
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-08-08 08:15:40 -0600

    Another update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import Home from './home';
+import Home from './home.js';
+import Menu from './menu.js';
+import Contact from './contact.js';
 
 const content = document.getElementById('content');
 const homeBtn = document.getElementById('homeBtn');
 const menuBtn = document.getElementById('menuBtn');
 const contactBtn = document.getElementById('contactBtn');
 const buttons = [homeBtn, menuBtn, contactBtn];
+
+let currentPage = Home();
+content.appendChild(currentPage);
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        content.removeChild(currentPage);
+        if (button === homeBtn) {
+            currentPage = Home();
+        } else if (button === menuBtn) {
+            currentPage = Menu();
+        } else if (button === contactBtn) {
+            currentPage = Contact();
+        }
+        content.appendChild(currentPage);
+    });
+});
