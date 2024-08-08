ebc7fd41cf494009bad3567d2970d9a282417480
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-08-08 08:12:09 -0600

    Created the home page

---
+++ src/home.js
@@ -0,0 +1,25 @@
+import './style.css';
+
+const Home = () => {
+    const content = document.createElement('div');
+
+    const header = document.createElement('h1');
+    header.innerHTML = '<h1>The Burger Joint</h1>';
+    header.classList.add('restTitle');
+    content.appendChild(header);
+
+    const headline = document.createElement('div');
+    headline.textContent = 'Home of the best burgers in town!';
+    headline.classList.add('headline');
+    content.appendChild(headline);
+
+    const image = document.createElement('img');
+    image.src = 'images/burger.jpeg';
+    image.alt = 'burger';
+    image.classList.add('burgerImg');
+    content.appendChild(image);
+
+    return content;
+}
+
+export default Home;
