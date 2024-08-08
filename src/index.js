b87e32e67f9fe8c6950c9b11ee0deac54f1fb724
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-08-08 08:14:06 -0600

    Update

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Home from './home';
+
+const content = document.getElementById('content');
+const homeBtn = document.getElementById('homeBtn');
+const menuBtn = document.getElementById('menuBtn');
+const contactBtn = document.getElementById('contactBtn');
+const buttons = [homeBtn, menuBtn, contactBtn];
